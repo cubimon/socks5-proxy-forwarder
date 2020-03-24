3b8cf5dabd30216f4fc17d3fab9fa3c86a5de4fb
cubimon <cubimon93@gmail.com>
2020-03-24 22:04:40 +0100

    import from

--- requesthandler.py
+++ requesthandler.py
@@ -1,12 +1,10 @@
 #!/bin/python3
 import logging
-import struct
-import psutil
+from struct import pack, unpack
+from psutil import net_connections, Process
 from enum import IntEnum
 from ipaddress import IPv4Address, IPv6Address
 from select import select
-from socket import AF_INET, AF_INET6, SOCK_STREAM
-from socket import inet_aton
 from socketserver import StreamRequestHandler
 from socks import socksocket
 from bandwidthmonitor import BandwidthMonitor
@@ -16,10 +14,10 @@ SOCKS_VERSION = 5
 
 
 def get_process_name(address, port):
-    for connection in psutil.net_connections():
+    for connection in net_connections():
         addr = connection.laddr
         if addr.ip == address and addr.port == port:
-            return psutil.Process(connection.pid).name()
+            return Process(connection.pid).name()
 
 
 class Method(IntEnum):
@@ -54,7 +52,7 @@ class RequestHandler(StreamRequestHandler):
             logging.info('accepting connection from %s' % process_name)
 
         # get version/authentication method count
-        version, nmethods = struct.unpack('!BB', self.connection.recv(2))
+        version, nmethods = unpack('!BB', self.connection.recv(2))
         if version != SOCKS_VERSION or nmethods <= 0:
             logging.warn('got wrong version from request %d', version)
             return
@@ -78,7 +76,7 @@ class RequestHandler(StreamRequestHandler):
             method = Method.NO_AUTH
 
         # send chosen authentication method
-        self.connection.sendall(struct.pack('!BB', SOCKS_VERSION, method))
+        self.connection.sendall(pack('!BB', SOCKS_VERSION, method))
 
         # authentication if required
         if self.requires_authentication and not self.verify_credentials():
@@ -86,8 +84,7 @@ class RequestHandler(StreamRequestHandler):
             return
 
         # address/port
-        version, cmd, _, address_type = \
-                struct.unpack('!BBBB', self.connection.recv(4))
+        version, cmd, _, address_type = unpack('!BBBB', self.connection.recv(4))
         if version != SOCKS_VERSION:
             logging.warn('got wrong version from request %d', version)
             return
@@ -101,7 +98,7 @@ class RequestHandler(StreamRequestHandler):
             address = IPv6Address(self.connection.recv(16))
         else:
             logging.warn('unknown address type %d' % address_type)
-        port = struct.unpack('!H', self.connection.recv(2))[0]
+        port = unpack('!H', self.connection.recv(2))[0]
         logging.info('requested connection to %s %d', address, port)
 
         # create connection
@@ -117,13 +114,13 @@ class RequestHandler(StreamRequestHandler):
                 logging.error('unknown command %d', cmd)
                 self.server.close_request(self.request)
                 return
-            reply = struct.pack('!BBBB', SOCKS_VERSION, 0, 0, address_type)
+            reply = pack('!BBBB', SOCKS_VERSION, 0, 0, address_type)
             if address_type in [AddressType.IPV4, AddressType.IPV6]:
                 reply += address.packed
             elif address_type == AddressType.DOMAINNAME:
-                reply += struct.pack('!B', len(address))
+                reply += pack('!B', len(address))
                 reply += address.encode('utf-8')
-            reply += struct.pack('!H', port)
+            reply += pack('!H', port)
         except Exception as err:
             logging.error(err)
             # return connection refused error
@@ -155,19 +152,18 @@ class RequestHandler(StreamRequestHandler):
 
         if username == self.username and password == self.password:
             # success, status = 0
-            response = struct.pack('!BB', version, 0)
+            response = pack('!BB', version, 0)
             self.connection.sendall(response)
             return True
 
         # failure, status != 0
-        response = struct.pack('!BB', version, 0xFF)
+        response = pack('!BB', version, 0xFF)
         self.connection.sendall(response)
         self.server.close_request(self.request)
         return False
 
     def generate_failed_reply(self, address_type, error_number):
-        return struct.pack('!BBBBIH', SOCKS_VERSION, error_number, 0,
-                           address_type, 0, 0)
+        return pack('!BBBBIH', SOCKS_VERSION, error_number, 0, address_type, 0, 0)
 
     def exchange_loop(self, client, remote, process_name, address):
         while True:
